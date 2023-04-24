5f9d918b3fae8bfa2e224699bfb6a382824528b1
nikrp <76831568+nikrp@users.noreply.github.com>
2023-04-24 07:48:53 -0700

    remove email id.

--- quiz_ui.py
+++ quiz_ui.py
@@ -233,7 +233,7 @@ class ComputerScienceA():
         """Send the email to the user's entered email address."""
         
         # Save the Reciever's Email Address
-        self.sender = "syrupisgreatagain@gmail.com"
+        self.sender = "<youremailid-here>@gmail.com"
         self.reciever = self.email_input.get()
         
         # Read the Image
@@ -257,7 +257,7 @@ class ComputerScienceA():
         s.ehlo()
         s.starttls()
         s.ehlo()
-        s.login(user=self.sender, password="zsrawagbeinwcupf")
+        s.login(user=self.sender, password="<your app password>")
         
         # Try Sending the Email
         try:
